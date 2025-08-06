11781df8d837e553e5fbbe6a9f02d7cadcdb511c
Leonardo Medina <leonardo1998ms91@gmail.com>
2025-08-05 19:49:02 -0500

    Actualiza el app

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+//Añadir amigos
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value;
+
+    if (!nombreAmigo) {
+        alert('Ingresa algún nombre');
+        return;
+    } else {
+        //Añadimos el nombre capturado a la lista amigos
+        amigos.push(nombreAmigo);
+
+        //Mostrar lista de amigos
+        mostrarAmigos();
+
+        console.log(amigos);
+    }
+}
